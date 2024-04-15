e41f8165d6d68968334941bf2440004ca3261024
mohammad hijazi <mohammad.hijazi.2000@gmail.com>
2024-04-15 13:54:34 +0300

    req fixes

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -1,9 +1,16 @@
   // IndexPage.js
   import React from 'react';
   import useSWR from 'swr';
+  import axios from 'axios';
 
-  const fetcher = (...args) => fetch(...args).then(res => res.json());
-
+  // const fetcher = (...args) => fetch(...args).then(res => res.json());
+  export const fetcher = (url: string) => axios.get(url, {
+    headers: {
+      'X-RapidAPI-Key': 'b77cc0403dmshd56eb519180ee3ep135e81jsnd98ccf1c86dc',
+      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+    }
+  }).then(res => res.data);
+  
   function IndexPage() {
     const { data, error } = useSWR(
       'https://google-translate1.p.rapidapi.com/language/translate/v2/languages',
--- src/TranslatePage.js
+++ src/TranslatePage.js
@@ -2,30 +2,38 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+
+const headers = {
+  'X-RapidAPI-Key': 'b77cc0403dmshd56eb519180ee3ep135e81jsnd98ccf1c86dc',
+  'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
+};
+
 function TranslatePage() {
   const [text, setText] = useState('');
   const [translatedText, setTranslatedText] = useState('');
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false)
+
+  const params = new URLSearchParams({
+    q: text,
+    source: 'en',
+    target: 'es'
+  });
+  
 
   const handleTranslate = async () => {
     try {
-      const response = await axios.get(
-        `https://google-translate1.p.rapidapi.com/language/translate/v2`,
-        {
-          params: {
-            q: text,
-            source: 'en',
-            target: 'es'
-          },
-          headers: {
-            'X-RapidAPI-Key': 'b77cc0403dmshd56eb519180ee3ep135e81jsnd98ccf1c86dc',
-            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-          }
-        }
-      );
+      setLoading(true)
+      setTranslatedText(false)
+      const response = await axios.post(url, params.toString(), { headers });
       setTranslatedText(response.data.data.translations[0].translatedText);
+      setError("")
     } catch (error) {
       setError('Error translating text!');
+    }
+    finally {
+      setLoading(false)
     }
   };
 
@@ -33,7 +41,8 @@ function TranslatePage() {
     <div>
       <h1>Translation</h1>
       <input type="text" value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={handleTranslate}>Translate</button>
+      <button disabled={loading} onClick={handleTranslate}>Translate</button>
+      {loading && <p>loading ...</p>}
       {translatedText && <div>Translated Text: {translatedText}</div>}
       {error && <div>{error}</div>}
     </div>
